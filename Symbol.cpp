5fe54d43f65516927e00cf69e48f07c4ad836afa
mbonkoungou <mathis.bonkoungou@insa-lyon.fr>
2025-03-05 22:20:44 +0100

    Version 3

--- Symbol.cpp
+++ Symbol.cpp
@@ -10,7 +10,37 @@ Symbol::Symbol(int id) : ident(id) {}
 Symbol::~Symbol() {}
 
 void Symbol::print() {
-    std::cout << "Symbol ID: " << ident << std::endl;
+    cout << "Symbol ID: " << ident << endl;
+}
+
+
+
+Addition::Addition() : Symbol(ADDITION) {}
+
+Addition::~Addition() {}
+
+void Addition::print() {
+    cout << "Symbol: +" << endl;
+}
+
+
+
+Multiplication::Multiplication() : Symbol(MULTIPLICATION) {}
+
+Multiplication::~Multiplication() {}
+
+void Multiplication::print() {
+    cout << "Symbol: *" << endl;
+}
+
+
+
+OpeningParenthesis::OpeningParenthesis() : Symbol(OPENINGPARENTHESIS) {}
+
+OpeningParenthesis::~OpeningParenthesis() {}
+
+void OpeningParenthesis::print() {
+    cout << "Symbol: (" << endl;
 }
 
 
@@ -41,14 +71,47 @@ Expr::~Expr() {}
 
 
 
+Value::~Value() {}
+
 Value::Value(double v) : number(v) {}
 
-double Value::eval(const std::map<std::string, double>& values) {
+double Value::eval(const map<string, double>& values) {
     return number;
 }
 
-void Value::display() {
-    std::cout << number;
+void Value::print() {
+    cout << number;
 }
 
 
+
+ExprBin::ExprBin(Expr* l, Expr* r) : left(l), right(r) {}
+
+ExprBin::~ExprBin() {
+    delete left;
+    delete right;
+}
+
+
+
+PlusExpr::PlusExpr(Expr* l, Expr* r) : ExprBin(l, r) {}
+
+double PlusExpr::eval(const map<string, double>& vals) {
+    return left->eval(vals) + right->eval(vals);
+}
+
+void PlusExpr::print() {
+    cout << "("; left->print(); cout << " + "; right->print(); cout << ")";
+}
+
+
+
+TimesExpr::TimesExpr(Expr* l, Expr* r) : ExprBin(l, r) {}
+
+double TimesExpr::eval(const map<string, double>& vals) {
+    return left->eval(vals) * right->eval(vals);
+}
+
+void TimesExpr::print() {
+    cout << "("; left->print(); cout << " * "; right->print(); cout << ")";
+}
--- Symbol.h
+++ Symbol.h
@@ -22,6 +22,42 @@ class Symbol {
 
 
 
+class Addition : public Symbol {
+    public:
+        Addition();
+        virtual ~Addition();
+        void print() override;
+    
+    protected:
+
+};
+    
+
+
+class Multiplication : public Symbol {
+    public:
+        Multiplication();
+        virtual ~Multiplication();
+        void print() override;
+    
+    protected:
+
+};
+    
+
+
+class OpeningParenthesis : public Symbol {
+    public:
+        OpeningParenthesis();
+        virtual ~OpeningParenthesis();
+        void print() override;
+
+    protected:
+
+};
+    
+    
+
 class ClosingParenthesis : public Symbol {
     public:
         ClosingParenthesis();
@@ -51,19 +87,61 @@ class Expr : public Symbol {
         Expr();
         virtual ~Expr();
         virtual double eval(const map<string, double>& values) = 0;
-        virtual void display() = 0;
+        virtual void print() override = 0;
+    
+    protected:
+
 };
 
 
 
 class Value : public Expr {
-    private:
-        double number;
     public:
         Value(double v);
-        ~Value() {}
+        ~Value();
         double eval(const map<string, double>& values) override;
-        void display() override;
+        void print() override;
+    
+    protected:
+        double number;
+
+};
+
+
+
+class ExprBin : public Expr {    
+    public:
+        ExprBin(Expr* l, Expr* r);
+        virtual ~ExprBin();
+
+    protected:
+        Expr* left;
+        Expr* right;
+
+};
+
+
+
+class PlusExpr : public ExprBin {
+    public:
+        PlusExpr(Expr* l, Expr* r);
+        double eval(const std::map<std::string, double>& values) override;
+        void print() override;
+    
+    protected:
+
+};
+
+
+
+class TimesExpr : public ExprBin {
+    public:
+        TimesExpr(Expr* l, Expr* r);
+        double eval(const std::map<std::string, double>& values) override;
+        void print() override;
+
+    protected:
+
 };
 
 
